e81ef0c0656aef39b4c18963e60b3b022fb0b9c5
raflirafli1876-dot@github.com <raflirafli1876@gmail.com>
2026-08-15 10:43:28 +0700

    fix: add vercel preset and set output dir

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,19 @@
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import tailwindcss from '@tailwindcss/vite'   // <-- WAJIB!
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
-    tailwindcss(),   // <-- HARUS ADA!
+    tailwindcss(),
     viteReact(),
   ],
-  css: {
-    minify: false,   // Matiin minify biar gak kena LightningCSS
-  },
-  resolve: {
-    tsconfigPaths: true,
-  },
-  build: {
-    cssMinify: false,
-  },
+  css: { minify: false },
+  resolve: { tsconfigPaths: true },
+  build: { cssMinify: false },
+  // ⬇️ INI YANG LO KURANG! ⬇️
+  server: {
+    preset: 'vercel'
+  }
 })
