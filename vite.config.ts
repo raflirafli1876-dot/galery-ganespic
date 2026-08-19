4458df52d3610af52bda7cc79340dd6660d1e945
raflirafli1876-dot@github.com <raflirafli1876@gmail.com>
2026-08-19 07:28:43 +0700

    fix: resolve build configurations, update TanStack start dependencies and add .npmrc for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,22 @@
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
+    tsconfigPaths(),
     tailwindcss(),
     viteReact(),
+    nitro(),
   ],
   css: {
     minify: false,
   },
-  resolve: {
-    tsconfigPaths: true,
-  },
   build: {
     cssMinify: false,
-  },
-  server: {
-    preset: 'vercel',
   },
 })
