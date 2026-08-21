ca54f76b482b7564c0c1f07b0368f562ced8bfb6
raflirafli1876-dot@github.com <raflirafli1876@gmail.com>
2026-08-21 20:42:50 +0700

    fix semua problem dan akhirnya gw bisa tidur nyenyak woiiii setelah kesekian error

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
+import viteReact from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
 
 export default defineConfig({
   plugins: [
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tanstackStart(),
     tsconfigPaths(),
     tailwindcss(),
     viteReact(),
-    nitro(),
   ],
   css: {
     minify: false,
